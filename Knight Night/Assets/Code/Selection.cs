cb1f6728669c2b78efa20408e3ecfa4ab4db17eb
Zygnius <twjia1998@gmail.com>
2018-09-23 17:27:18 -0400

    Basic menu layout

---
+++ Knight Night/Assets/Code/Selection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selection : MonoBehaviour {
+
+    public string[] names;
+
+    private int currIndex;
+
+
+    public int getSelection()
+    {
+        return currIndex;
+    }
+}
