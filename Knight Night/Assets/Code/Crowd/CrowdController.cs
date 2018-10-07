157f5ce1a0977c0c37a43547007cb3043680b501
Feng Wu <thewestinggame@gmail.com>
2018-10-07 15:27:08 -0400

    crowd init

--- Knight Night/Assets/Code/Crowd/CrowdController.cs
+++ Knight Night/Assets/Code/Crowd/CrowdController.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class CrowdController : MonoBehaviour
 {
 
+    //adjust this to change speed
+    public float speed;
+    //adjust this to change how high it goes
+    public float height;
+
     // Use this for initialization
     void Start()
     {
@@ -13,6 +18,11 @@ public class CrowdController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //get the objects current position and put it in a variable so we can access it later with less code
+        Vector3 pos = transform.position;
+        //calculate what the new Y position will be
+        float newY = Mathf.Sin(Time.time * speed);
+        //set the object's Y to the new calculated Y
+        transform.position = new Vector3(pos.x, newY, pos.z) * height;
     }
 }
