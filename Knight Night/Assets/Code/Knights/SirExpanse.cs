15805fea3ad5257e7db7f1167a9629d469b64582
gaminkid05 <tsanmigu@gmail.com>
2018-11-11 16:47:19 -0500

    Added Sir Expanse

---
+++ Knight Night/Assets/Code/Knights/SirExpanse.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SirExpanse : Knight
+{
+    public GameObject lightsaber;
+    public float jumpForce;
+    public float gravity;
+    public float floatingSpeed;
+
+    void Start()
+    {
+        start();
+        if (combatant == null)
+        {
+            Debug.Log("Sir Lance requires Combatant component");
+        }
+        
+        _groundY = transform.position.y;
+    }
+
+    void Update()
+    {
+        update();
+        //Jump Logic
+        if (Input.GetButtonDown(combatant.jumpButton) && transform.position.x > combatant.positionBounds.x && transform.position.x < combatant.positionBounds.y)
+        {
+            if (transform.position.y == _groundY)
+            {
+                _vSpeed = jumpForce;
+            }
+        }
+
+        if (Input.GetButton(combatant.jumpButton))
+        {
+            _vSpeed = Mathf.Max(floatingSpeed * -1, _vSpeed);
+        }
+
+        transform.Translate(0, _vSpeed * Time.deltaTime, 0);
+
+        if (_vSpeed != 0)
+        {
+            _vSpeed = _vSpeed - gravity * Time.deltaTime;
+        }
+
+        if (transform.position.y < _groundY)
+        {
+            _vSpeed = 0;
+            transform.position = new Vector3(transform.position.x, _groundY, 0);
+        }
+    }
+    public override void throwWeapon(float aimDir)
+    {
+        GameObject saber = Instantiate(lightsaber);
+        saber.GetComponent<LightSaber>()._otherKnight = combatant.opposingKnight;
+        saber.transform.position = transform.position;
+        saber.transform.Rotate(0, 0, aimDir);
+    }
+
+    //Dummy
+    public override void turnAround()
+    {
+        return;
+    }
+}
---
+++ Knight Night/Assets/Code/Weapons/LightSaber.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LightSaber : Weapon
+{
+    [SerializeField]
+    private float _speed;
+
+    void Start()
+    {
+        start();
+    }
+    void Update()
+    {
+        update();
+        previousPosition = transform.position;
+        if (!stopped)
+        {
+            transform.Translate(_speed * Time.deltaTime, 0, 0);
+        }
+
+        Vector3 moveDirection = transform.position - previousPosition;
+        if (moveDirection != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        onTriggerEnter2D(other);
+    }
+}
+
