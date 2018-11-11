86fc9b27e961f50af4fa4c8516f2042f260525f5
angelagyang <agyang@andrew.cmu.edu>
2018-11-11 16:00:29 -0500

    all sound effects accessed through AudioSource array

--- Knight Night/Assets/Code/SoundEffectsManager.cs
+++ Knight Night/Assets/Code/SoundEffectsManager.cs
@@ -7,42 +7,13 @@ using UnityEngine.SceneManagement;
 public class SoundEffectsManager : MonoBehaviour {
 
     public static SoundEffectsManager instance = null;
-    
-    public AudioSource buttonDeselect;
-    public AudioSource buttonDown;
-    public AudioSource buttonSelect;
-    public AudioSource buttonUp;
-    public AudioSource cheerAmbient;
-    public AudioSource cheerExcite;
-    public AudioSource gallop;
-    public AudioSource lanceHitGround;
-    public AudioSource lanceHitKnight;
-    public AudioSource recordHitGround;
-    public AudioSource recordHitKnight;
-    public AudioSource rocketExplode;
-    public AudioSource rocketShoot;
-    public AudioSource throwWeapon;
+
+    // each sound effect is assigned a number in the inspector
     public AudioSource[] Sounds = new AudioSource[14];
-   
+
     // Use this for initialization
-    void Start ()
-    {
-        // assign each sound effect a number 
-        Sounds[0] = buttonDeselect;
-        Sounds[1] = buttonDown;
-        Sounds[2] = buttonSelect;
-        Sounds[3] = buttonUp;
-        Sounds[4] = cheerAmbient;
-        Sounds[5] = cheerExcite;
-        Sounds[6] = gallop;
-        Sounds[7] = lanceHitGround;
-        Sounds[8] = lanceHitKnight;
-        Sounds[9] = recordHitGround;
-        Sounds[10] = recordHitKnight;
-        Sounds[11] = rocketExplode;
-        Sounds[12] = rocketShoot;
-        Sounds[13] = throwWeapon; 
-	}
+    void Start()
+    { }
 
     void Awake ()
     {
