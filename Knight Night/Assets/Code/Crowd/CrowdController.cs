06b158465359b1a7336999e186e58d6195b4de7a
angelagyang <agyang@andrew.cmu.edu>
2018-11-18 16:26:02 -0500

    added new art (not the excite art yet)

--- Knight Night/Assets/Code/Crowd/CrowdController.cs
+++ Knight Night/Assets/Code/Crowd/CrowdController.cs
@@ -9,40 +9,40 @@ public class CrowdController : MonoBehaviour
 
     // Use this for initialization
     void Start()
-    {
-
-    }
+    { }
 
     // Update is called once per frame
     void Update()
     {
-        //System.Random rand = new System.Random();
         string parentName = transform.parent.name;
+
         //get the objects current position and put it in a variable so we can access it later with less code
         Vector3 pos = transform.position;
         float newY = 0;
-        if (parentName == "RedCrowd")
+
+        if (parentName == "knight1Crowd")
         {
-            float crowdThreshold = parent.redExcitement*transform.parent.childCount;
+            float crowdThreshold = parent.knight1Excitement * transform.parent.childCount;
 
             if (id < crowdThreshold)
             {
                 //calculate what the new Y position will be
-                newY = Mathf.Sin(Time.time * parent.speed * parent.redExcitement + id);
+                newY = Mathf.Sin(Time.time * parent.speed * parent.knight1Excitement + id);
             }
             //set the object's Y to the new calculated Y
-            transform.position = new Vector3(pos.x, parent.redHeight + newY * parent.amp, pos.z);
+            transform.position = new Vector3(pos.x, parent.knight1Height + newY * parent.amp, pos.z);
         }
+        // knight2 crowd 
         else
         {
-            float crowdThreshold = parent.blueExcitement*transform.parent.childCount;
+            float crowdThreshold = parent.knight2Excitement * transform.parent.childCount;
 
             if (id < crowdThreshold)
             {
-                newY = Mathf.Sin(Time.time * parent.speed * parent.blueExcitement);
+                newY = Mathf.Sin(Time.time * parent.speed * parent.knight2Excitement);
             }
             //set the object's Y to the new calculated Y
-            transform.position = new Vector3(pos.x, parent.blueHeight + newY * parent.amp, pos.z);
+            transform.position = new Vector3(pos.x, parent.knight2Height + newY * parent.amp, pos.z);
         }
         
     }
--- Knight Night/Assets/Code/Crowd/CrowdGroup.cs
+++ Knight Night/Assets/Code/Crowd/CrowdGroup.cs
@@ -4,20 +4,16 @@ using UnityEngine;
 
 public class CrowdGroup : MonoBehaviour {
 
-    public float redExcitement;
-    public float blueExcitement;
-    public float redHeight;
-    public float blueHeight;
+    public float knight1Excitement;
+    public float knight2Excitement;
+    public float knight1Height;
+    public float knight2Height;
     public float speed;
     public float amp;
 
     // Use this for initialization
-    void Start () {
-		
-	}
+    void Start () {}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update () {}
 }
