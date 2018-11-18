28ded3a266d35886fd5b240cf995289685742e40
Zygnius <twjia1998@gmail.com>
2018-11-18 17:12:44 -0500

    Menu updates, Char select bug fix, game ui

--- Knight Night/Assets/Code/UI/CharacterSelectManager.cs
+++ Knight Night/Assets/Code/UI/CharacterSelectManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CharacterSelectManager : MonoBehaviour {
 
@@ -8,30 +9,18 @@ public class CharacterSelectManager : MonoBehaviour {
     public ScrollSelector p1Selector;
     public ScrollSelector p2Selector;
 
-    //Player Specific EventSystems
-    public MultiEventSystem player1;
-    public MultiEventSystem player2;
-
     //Character selects
     [SerializeField] private CharacterSelect p1Select;
     [SerializeField] private CharacterSelect p2Select;
 
     //Player Knight Choice
     [SerializeField] private PlayerChoiceData playerChoice;
-
-
-    private void Awake()
-    {
-        player1.SetSelectedGameObject(p1Selector.gameObject);
-        player2.SetSelectedGameObject(p2Selector.gameObject);
-    }
+    
 
     private void Update()
     {
         if (p1Select.ReadyCheck() && p2Select.ReadyCheck())
         {
-            player1.gameObject.SetActive(false);
-            player2.gameObject.SetActive(false);
             p1Select.enabled = false;
             p2Select.enabled = false;
             playerChoice.player1 = p1Selector.GetSelectedKnight();
@@ -42,8 +31,6 @@ public class CharacterSelectManager : MonoBehaviour {
 
     public void BackToMainMenu()
     {
-        player1.gameObject.SetActive(false);
-        player2.gameObject.SetActive(false);
         p1Select.enabled = false;
         p2Select.enabled = false;
         LoadManager.instance.LoadScene("Menu");
